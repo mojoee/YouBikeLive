db40587bd93ea503a34a1ef1d474a462bdc3b415
david <wolledav@cvut.cz>
2024-11-28 17:16:14 +0800

    instance transformation  to unit demand and back added

---
+++ optimization/utils/generate_unit_instance.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import json
+import numpy as np
+import sys
+
+# PARAMETERS
+instance_path = sys.argv[1]
+output_path = instance_path.replace(".json", "_unit.json")
+
+
+# LOAD
+with open(instance_path, 'r') as file:
+    data = json.load(file)
+
+
+# GENERATE UNIT INSTANCE
+# 1) Generate stations with unit demand
+stations_new = []
+stCnt_new = 0
+stations = data["stations"]
+for station in stations:
+    demand = station["s_goal"] - station["s_init"]
+    if demand == 0:
+        continue
+    demand_new = np.sign(demand)
+    for i in range(abs(demand)):
+        station_new = {"id": stCnt_new, "parent_id": station["id"]}
+        stCnt_new += 1
+        if demand_new > 0:
+            station_new["s_init"] = 0
+            station_new["s_goal"] = 1
+        else:
+            station_new["s_init"] = 1
+            station_new["s_goal"] = 0
+        stations_new.append(station_new)
+print("Generated stations_new")
+
+# 2) Generate distance matrix
+distances = data["distances"]
+distances_new = []
+for i in range(stCnt_new):
+    distances_row = [None] * stCnt_new
+    for j in range(stCnt_new):
+        dist = distances[stations_new[i]["parent_id"]][stations_new[j]["parent_id"]]
+        distances_row[j] = int(dist)
+    distances_new.append(distances_row)
+print("Generated distances_new")
+
+# 3) Generate depot
+depot = data["depot"]
+depot_new = {}
+dfd_new = []
+dtd_new = []
+for station in stations_new:
+    dfd_new.append(int(depot["dists_from_depot"][station["parent_id"]]))
+    dtd_new.append(int(depot["dists_to_depot"][station["parent_id"]]))
+depot_new["dists_from_depot"] = dfd_new
+depot_new["dists_to_depot"] = dtd_new
+print("Generated depot_new")
+
+# 4) Carry over vehicles
+vehicles_new = data["vehicles"]
+
+# Export unit instance
+data_new = {
+    "depot": depot_new,
+    "stations": stations_new, 
+    "distances": distances_new, 
+    "vehicles": vehicles_new
+}
+print("data_new prepared")
+
+with open(output_path, "w") as outfile:
+    json.dump(data_new, outfile, indent=None)
+    print("Exported instance", output_path)
---
+++ optimization/utils/process_unit_solution.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import json
+import numpy as np
+import sys
+
+# PARAMETERS
+instance_path = "./data/instances/demo_unit.json"
+solution_path = "./results/demo_unit.json"
+
+for i in range(len(sys.argv)):
+    if sys.argv[i] == '-i':
+        instance_path = sys.argv[i+1]
+    elif sys.argv[i] == '-s':
+        solution_path = sys.argv[i+1]
+
+# LOAD
+data = {}
+with open(instance_path, 'r') as file:
+    data = json.load(file)
+
+solution = {}
+with open(solution_path, 'r') as file:
+    solution = json.load(file)
+
+for rt in solution["routes"]:
+    route = rt["route"]
+    # route_parents = [data["stations"][node]["parent_id"] for node in route]
+    # print(route_parents)
+    loads = rt["leaving_load"]
+    # print(loads)    
+    route_merged = [data["stations"][route.pop()]["parent_id"]]
+    loads_merged = [loads.pop()]
+    while(len(route) > 0):
+        node = route.pop()
+        load = loads.pop()
+        node_parent = data["stations"][node]["parent_id"]
+        if route_merged[-1] != node_parent:
+            route_merged.append(node_parent)
+            loads_merged.append(load)
+    route_merged.reverse()
+    loads_merged.reverse()
+
+    print(route_merged)
+    print(loads_merged)
+
+    # TODO check, export
