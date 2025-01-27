2da35711154a4d1e0edc022cd300614ce8ef7975
david <wolledav@cvut.cz>
2025-01-27 16:54:05 +0800

    first batch of results removed, new experiment setup

--- optimization/run.sh
+++ optimization/run.sh
@@ -47,24 +47,3 @@ for INSTANCE in "$INSTANCE_DIR"*.json; do
     echo "Solving instance: $INSTANCE"
     python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT
 done
-
-
-
-
-# INSTANCE=./data/instances_v4/v12-24-24_b8h_uniform_goal.json
-# TIMEOUT=3600
-
-# OUTPUT=./results/v4/
-# python3 ./optimization/models/rebalance_v4.py -i $INSTANCE -o $OUTPUT -t $TIMEOUT
-
-# OUTPUT=./results/v4_unit/
-# python3 ./optimization/models/rebalance_unit.py -i $INSTANCE -o $OUTPUT -t $TIMEOUT -f v4
-
-# OUTPUT=./results/v4_cb/
-# python3 ./optimization/models/rebalance_unit.py -i $INSTANCE -o $OUTPUT -t $TIMEOUT -f v4_cb
-
-# OUTPUT=./results/v4_cbws/
-# python3 ./optimization/models/rebalance_v4_cbws.py -i $INSTANCE -o $OUTPUT -t $TIMEOUT
-
-# OUTPUT=./results/v5_1/
-# python3 ./optimization/models/rebalance_v5_1.py -i $INSTANCE -o $OUTPUT -t $TIMEOUT
