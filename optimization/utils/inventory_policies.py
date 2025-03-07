c5e225af694d8d1fde6cd7d433c783e7f556996c
david <wolledav@cvut.cz>
2025-03-07 14:38:03 +0100

    policies demo

--- optimization/utils/inventory_policies.py
+++ optimization/utils/inventory_policies.py
@@ -10,6 +10,7 @@ def get_inventories(demands, s_init, s_cap):
         inventories[t] = min(max(0, inventories[t-1] + demands[t-1]), s_cap)
     return inventories
 
+
 # Get peak unmet demand, positive or negative
 def get_P_max(demands, inventories, s_cap):
     n = len(demands)
@@ -25,6 +26,7 @@ def get_P_max(demands, inventories, s_cap):
     # print("P_short:", P_short)
     P_max = max(P_over, P_short)
     return P_max
+
 
 # Get total unmet demand, positive and negative
 def get_Q_total(demands, inventories, s_cap):
@@ -60,7 +62,6 @@ def min_P_max_policy(demands, s_cap):
     return s_init
 
 
-
 # Returns s_init, so that Q_total in minimal
 def min_Q_total_policy(demands, s_cap):
     min_Q_total = float('inf')
@@ -80,17 +81,26 @@ def min_Q_total_policy(demands, s_cap):
     return s_init
 
 
-
 if __name__ == "__main__":
     np.random.seed(0)
     n = 12
     demands = np.random.randint(-15, 15, size=n)
-    s_cap = 30
+    s_cap = 20
 
-    s_init = 10
-    inventories = get_inventories(demands, s_init, s_cap)
-    P_max = get_P_max(demands, inventories, s_cap)
-    Q_total = get_Q_total(demands, inventories, s_cap)
+    # s_init = 10
+    # inventories = get_inventories(demands, s_init, s_cap)
+    # P_max = get_P_max(demands, inventories, s_cap)
+    # Q_total = get_Q_total(demands, inventories, s_cap)
 
-    min_P_max_policy(demands, s_cap)
-    min_Q_total_policy(demands, s_cap)
+    s_init_1 = min_P_max_policy(demands, s_cap)
+    inventories_1 = get_inventories(demands, s_init_1, s_cap)
+
+    s_init_2 = min_Q_total_policy(demands, s_cap)
+    inventories_2 = get_inventories(demands, s_init_2, s_cap)  
+
+    print("demands: ", demands)
+    print("s_cap: ", s_cap)
+    print("s_init_1: ", s_init_1)
+    print("inventories_1: ", inventories_1)
+    print("s_init_2: ", s_init_2)
+    print("inventories_2: ", inventories_2)
